14c280c4a1bee18eb4f37b49694e576dfe00e0da
reeocho <ziyao.chang@gmail.com>
2026-05-13 10:34:34 +1000

    basic start screen with text input

--- main.py
+++ main.py
@@ -5,15 +5,30 @@ from time import *
 
 iteration = 0
 
-# create game window
+def text_create(text, settings):
+    texttodraw = Text(Point(200, 50), text)
+    texttodraw.setTextColor("red")
+    texttodraw.draw(settings)
+    sleep(5)
+    texttodraw.undraw()
+
+
 def main():
-    settings = GraphWin("Memory Game", 1728, 1080)
-    button = Rectangle(Point(250, 100), Point(350, 150)).draw(settings)
-    button_text = Text(Point(300, 125), "Start").draw(settings)
+    settings = GraphWin("Memory Game", 500, 300)
+    e = Entry(Point(250, 150), 50)
+    e.draw(settings)
+    button = Rectangle(Point(200, 200), Point(300, 250))
+    button.label = Text(Point(250, 225), "Start")
+    button.label.draw(settings)
+    button.draw(settings)
     while True:
-        if button_text.getText() == "Start":
+        text = e.getText()
+        if button.getP1().getX() < settings.getMouse().getX() < button.getP2().getX() and \
+           button.getP1().getY() < settings.getMouse().getY() < button.getP2().getY():
             settings.close()
             break
+        else:
+            text_create("Type 'start' to begin the game!", settings)
 
 def create_dot_grid(game_code, number):
     spacing = 50
@@ -26,18 +41,18 @@ def create_dot_grid(game_code, number):
 
     for i, x in enumerate(range(spacing, width, spacing)):
         for j, y in enumerate(range(spacing, width, spacing)):
-            grid_id = f"{i}-{j}"
-            globals()['var%s' % grid_id] = Circle(Point(x, y), 2)
-            globals()['var%s' % grid_id].draw(win)
-            
-            dots.append(grid_id)
+            circle = Circle(Point(x, y), 5)
+            circle.draw(win)
+            dots.append(circle)
             
     return win, dots
 
 
-    
+
 main()
+
+
+
 win, dots = create_dot_grid("Dot Grid", 5)
-print(dots)
 win.getMouse()
 win.close()
